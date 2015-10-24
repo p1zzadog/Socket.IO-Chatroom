f390396a8dbbb5e5dcae93815295a543dfbc235c
Steven A Moody <steve.moody2@gmail.com>
2015-10-24 13:46:36 -0600

    implemented some angular-material design

--- public/js/ngMain.js
+++ public/js/ngMain.js
@@ -3,7 +3,7 @@ angular.module('chatApp', ['ngRoute', 'ngMaterial'])
 
 // configure
 angular.module('chatApp')
-	.config(['$routeProvider', function($routeProvider){
+	.config(['$routeProvider', '$mdThemingProvider', function($routeProvider, $mdThemingProvider){
 			$routeProvider
 				.when('/', {
 					templateUrl : 'html/views/home.html',
@@ -13,6 +13,11 @@ angular.module('chatApp')
 					templateUrl : 'html/views/login.html',
 					controller  : 'loginController as loginCtrl'
 				})
+
+			$mdThemingProvider.theme('default')
+				.primaryPalette('indigo')
+       			.accentPalette('grey')
+        		.warnPalette('red');
 	}]);
 
 // mainController
@@ -32,12 +37,15 @@ angular.module('chatApp')
 		mainCtrl.send = function(){
 			socket.emit('client-send', mainCtrl.message);
 			mainCtrl.message = '';
+			var chatScroll = document.getElementById("chat-window");
+			chatScroll.scrollTop = chatScroll.scrollHeight;
 		}
 
 		socket.on('server-send', function(returnMessages){
 			console.log('message was received');
 			$scope.$apply(function(){
 				mainCtrl.allMessages = returnMessages;
+				console.log(mainCtrl.allMessages);
 
 			});
 		})
